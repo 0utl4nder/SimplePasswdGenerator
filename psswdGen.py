f7788686fd6fbc225379e7dcf862d5745e08a31a
0utl4nder <outlander@localhost.localdomain>
2022-12-14 21:02:19 -0500

    First Version

--- psswdGen.py
+++ psswdGen.py
@@ -7,7 +7,6 @@ from tkinter.messagebox import showinfo
 import tkinter as tk
 
 root = tk.Tk()
-#root.geometry('250x120')
 root.title('Strongth Password Generator')
 leng = tk.StringVar()
 characteres = "#0a*G1bH2cI3dJ4eK5fL:6gM7hN8iO9jP=kQ¿lR?mS(nT)oU[pV]qW{rX}sY+tZ-u_v.w,x<y>z@A$;B€C^D!E¡F'"
@@ -23,17 +22,13 @@ class Content:
         label_entry = tk.Entry(entry, textvariable=leng)
         label_entry.pack(fill='x', expand=True)
         label_entry.focus()
-
-
-        test = tk.Label(root, text=leng.get())
         
         button2 = tk.Button(root,text="[ -Countinue- ]",command=Content.feedback)
         button2.pack()
 
         button = tk.Button(root,text="[!] Exit",command=root.destroy)
         button.pack()
-        
-        #self.leng = leng.get()
+
     def feedback():
         Password = ""
         try:
